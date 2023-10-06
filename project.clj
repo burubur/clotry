eab044394f98768e36fc59097b93463129b29805
Burhan Mubarok <burhan.mubarok@gojek.com>
2023-10-06 15:59:54 +0700

    Added project definition

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clotry "0.1.0-SNAPSHOT"
+  :description "A sample project to explore clojure features"
+  :url "https://https://github.com/burubur/clotry"
+  :license {:name "MIT"
+            :url "https://opensource.org/license/mit/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns clotry.core})
